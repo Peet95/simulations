03229f220ef96feecee000c18ecfab54c2986ed9
Peet95 <128177702+Peet95@users.noreply.github.com>
2023-09-03 10:00:25 +0200

    Update string.cpp

--- string/string.cpp
+++ string/string.cpp
@@ -2,21 +2,24 @@
 #include <vector>
 #include <fstream>
 #include <math.h>
-using namespace std;
+
 int main() 
 {
-  ofstream myfile;
+  std::ofstream myfile;
   myfile.open ("data.txt");
+
   // Parameters
   double t = 0.0;
   double rho   = 0.01;                                              
   double ten   = 40.0;                                             
   double c     = sqrt(ten/rho);                                  
-  double c1    = c;                                                 // CFL criterium
+  double c1    = c; // CFL criterium
   double ratio =  c*c/(c1*c1);
+
   // Initialization
   std::vector<std::vector<double>>  xi(101, std::vector<double>(3, 0));
-  vector<double> xa(100), ya(100);
+  std::vector<double> xa(100), ya(100);
+
   for(int i = 0; i < 81; i++) {
     xi[i][0] = 0.00125*i; 
   }        
@@ -26,7 +29,7 @@ int main()
   for(int i = 0; i < 100; i++) {
     xa[i] = 2.0 * i - 100.0;                            
     ya[i] = 300.0 * xi[i][0];  
-    myfile << t << " " << xa[i] << " " << ya[i] << endl;  
+    myfile << t << " " << xa[i] << " " << ya[i] << std::endl;  
   }
 
   // Later time steps
@@ -35,20 +38,20 @@ int main()
   }
   while (t < 80.0){
     for(int i = 1; i < 100; i++) {
-       xi[i][2] = 2.0*xi[i][1] - xi[i][0] + ratio * (xi[i+1][1] + xi[i-1][1]-2.0*xi[i][1]);
+      xi[i][2] = 2.0*xi[i][1] - xi[i][0] + ratio*(xi[i+1][1] + xi[i-1][1]-2.0*xi[i][1]);
 
     }
       
     for(int i = 1; i < 100; i++) {
-         xa[i] = 2.0*i - 100.0 ;                  
-         ya[i] = 300.0 * xi[i][2]  ;
-             myfile << t << " " << xa[i] << " " << ya[i] << endl; 
+      xa[i] = 2.0*i - 100.0;                  
+      ya[i] = 300.0 * xi[i][2];
+      myfile << t << " " << xa[i] << " " << ya[i] << std::endl; 
     }
     
    
     for(int i = 0; i < 101; i++) {
-        xi[i][0] = xi[i][1]  ;                              
-        xi[i][1] = xi[i][2] ;
+      xi[i][0] = xi[i][1];                              
+      xi[i][1] = xi[i][2];
     }
     
     t=t+1.0;
